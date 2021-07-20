f29d431bd8435b5f0cdc1d9587056f645d51201f
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2021-07-20 20:54:06 -0300

    refactor: move duplicated code to a helper method

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,12 @@
+import { HttpResponse } from '../protocols/http'
+
+const badRequest = (error: Error): HttpResponse => {
+  return {
+    statusCode: 400,
+    body: error
+  }
+}
+
+export {
+  badRequest
+}
