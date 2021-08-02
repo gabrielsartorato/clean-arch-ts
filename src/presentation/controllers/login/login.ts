ca205f67d9d4f2479c71fdfee17ebdd60adf464f
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2021-08-02 20:55:32 -0300

    refactor: remove duplicated code from LoginController

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -16,12 +16,13 @@ class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { email, password } = httpRequest.body
-      if (!email) {
-        return badRequest(new MissingParamError('email'))
-      }
 
-      if (!password) {
-        return badRequest(new MissingParamError('password'))
+      const requiredFields = ['email', 'password']
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
 
       const isValid = this.emailValidator.isValid(email)
