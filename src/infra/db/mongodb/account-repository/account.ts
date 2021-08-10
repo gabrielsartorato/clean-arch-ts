3da537982139ece8b681379f2b2087c0c614a010
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2021-08-09 21:14:28 -0300

    feat: ensure AccountMongoRepository loadByEmail return a emaill when success and null when no find anything

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,14 +2,23 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 
-class AccountMongoRepository implements AddAccountRepository {
+class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollections = await MongoHelper.getCollection('accounts')
 
     const result = await accountCollections.insertOne(accountData)
 
     return MongoHelper.map(result.ops[0])
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollections = await MongoHelper.getCollection('accounts')
+
+    const account = await accountCollections.findOne({ email })
+
+    return account && MongoHelper.map(account)
   }
 }
 
