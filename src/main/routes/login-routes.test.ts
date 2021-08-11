f21571f2b3bfeb5444cb9e8554b5ce1635a4ca0a
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2021-08-10 21:03:19 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should be able return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'gabriel@email.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
